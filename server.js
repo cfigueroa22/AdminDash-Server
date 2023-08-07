155e070868eb77bf0148801d7602ac1ed1292507
Carlos Figueroa <carfig7186@students.ecpi.edu>
2023-08-06 20:52:54 -0400

    cors updates

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ app.use(cookieParser());
 app.use(express.json());
 // app.use(express.static("public"));
 app.use(express.static("build"));
-app.use(cors());
+
+app.use(
+  cors({
+    origin: ["'https://admindash-server-production.up.railway.app'"],
+    methods: ["POST", "GET", "PUTS", "DELETE"],
+    credentials: true,
+  })
+);
 
 const db = mysql.createConnection({
   host: process.env.REACT_APP_MYSQL_HOST,
