dd5f1bab9330173bd883b512fe26b72ada40a553
Carlos Figueroa <carfig7186@students.ecpi.edu>
2023-08-06 22:11:12 -0400

    headers updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("build"));
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://admindash-server-production.up.railway.app"
+    "https://unrivaled-tapioca-a02bd0.netlify.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
